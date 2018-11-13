c33270b5628f08367604b2e73973887a80929bcf
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-13 13:44:35 -0500

    UPDATE: getting the data from API, done!

--- Startbase/starbase/src/App.vue
+++ Startbase/starbase/src/App.vue
@@ -2,7 +2,8 @@
     <div id="app">
         <div class="container mt-5">
             <h2>{{title}}</h2>    
-            <p>{{description}}</p>    
+            <p>{{description}}</p>   
+            <button type="button" class="btn btn-success" @click="fetchCharacter">Get the data</button> 
         </div>
     </div>
 </template>
@@ -14,6 +15,15 @@
             return {
                 title: 'Welcome to Starbase!',
                 description: 'Here you can find all information about your favorite star wars characters.'
+            }
+        },
+        methods: {
+            fetchCharacter(){
+                // Fetch the data from API
+                fetch('https://swapi.co/api/people/', {
+                    method: 'GET'
+                    //create a promise to respose the data from API
+                }).then(response => response.json()).then(json => console.log(json))
             }
         }
     }
