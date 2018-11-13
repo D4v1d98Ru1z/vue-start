ade0a394ea384b0e949bce04ed7c8d10a14d6c89
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-12 20:32:33 -0500

    UPDATE:  adding title

--- Startbase/starbase/src/App.vue
+++ Startbase/starbase/src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div id="app">
-
+        <h2>{{title}}</h2>
     </div>
 </template>
 
 <script>
     export default{
-        name: 'app'
+        name: 'app',
+        data(){
+            return {
+                title: 'Welcome to Vue project'
+            }
+        }
     }
 </script>
