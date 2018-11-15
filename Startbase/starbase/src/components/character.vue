72118d3a7edf4788b624d540f97b30edaea9e508
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-14 19:00:26 -0500

    UPDATE: creating the character component

---
+++ Startbase/starbase/src/components/character.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{Character}}
+    </div>   
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                Character: {}
+            }
+        }
+    }
+</script>
