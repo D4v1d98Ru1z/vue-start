76e9491095c8854e6ccb2c12dc11875a1d8bea0d
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-14 19:18:54 -0500

    UPDATE: adding the character component

--- Startbase/starbase/src/App.vue
+++ Startbase/starbase/src/App.vue
@@ -3,12 +3,15 @@
         <div class="container mt-5">
             <h2>{{title}}</h2>    
             <p>{{description}}</p>   
-            <button type="button" class="btn btn-success" @click="fetchCharacter">Get the data</button> 
+            <Character/>
+            
         </div>
     </div>
 </template>
 
 <script>
+    import Character from './components/character.vue'
+
     export default{
         name: 'app',
         data(){
@@ -17,14 +20,8 @@
                 description: 'Here you can find all information about your favorite star wars characters.'
             }
         },
-        methods: {
-            fetchCharacter(){
-                // Fetch the data from API
-                fetch('https://swapi.co/api/people/', {
-                    method: 'GET'
-                    //create a promise to respose the data from API
-                }).then(response => response.json()).then(json => console.log(json))
-            }
+        components: {
+           Character 
         }
     }
 </script>
--- Startbase/starbase/src/components/character.vue
+++ Startbase/starbase/src/components/character.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
-        {{Character}}
+        {{character}}
     </div>   
 </template>
 <script>
     export default{
         data(){
             return{
-                Character: {}
+                character: {}
             }
         },
         methods:{
             fetchCharacter(){
                 // Fetch the data from API
-                fetch('https://swapi.co/api/people/', {
+                fetch('https://swapi.co/api/people/1', {
                     method: 'GET'
                     //create a promise to respose the data from API
-                }).then(response => response.json()).then(json => console.log(json))
+                }).then(response => response.json()).then(json => this.character = json)
             }
         },
         //created hook
