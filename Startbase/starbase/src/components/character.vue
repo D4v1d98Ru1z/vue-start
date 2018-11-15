0a02935f5f9b4f169407627d661a55f6983f8f26
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-14 19:10:40 -0500

    UPDATE: create a hook

--- Startbase/starbase/src/components/character.vue
+++ Startbase/starbase/src/components/character.vue
@@ -9,6 +9,20 @@
             return{
                 Character: {}
             }
+        },
+        methods:{
+            fetchCharacter(){
+                // Fetch the data from API
+                fetch('https://swapi.co/api/people/', {
+                    method: 'GET'
+                    //create a promise to respose the data from API
+                }).then(response => response.json()).then(json => console.log(json))
+            }
+        },
+        //created hook
+        created(){
+            this.fetchCharacter()
         }
+
     }
 </script>
