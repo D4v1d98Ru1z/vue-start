189c61a69cf5aaf75a3ac24e9e0535f5dbbd0e53
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-17 11:54:59 -0500

    UPDATE: import routes in main js

--- startbase-router/src/main.js
+++ startbase-router/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import VueRoutes from 'vue-router'
+Vue.use(VueRoutes)
 import App from './App.vue'
+
+const routes = [
+    {path: './data/:type'}
+]
 
 new Vue({
     el: '#app',
