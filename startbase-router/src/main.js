0596bd869161e5cc817f1a8df566e5c0dde2c6d7
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-17 11:59:48 -0500

    UPDATE: adding a new component

--- startbase-router/src/main.js
+++ startbase-router/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRoutes from 'vue-router'
 Vue.use(VueRoutes)
 import App from './App.vue'
+import Data from './components/Data.vue'
 
 const routes = [
     {path: './data/:type'}
