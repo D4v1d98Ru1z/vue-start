49ee81e611c8965f56455806cd5e2ba035c2471a
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-17 12:04:36 -0500

    UPDATE: fixing a mistake

--- startbase-router/src/main.js
+++ startbase-router/src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import Data from './components/Data.vue'
 
 const routes = [
-    {path: './data/:type', Data}
+    {path: '/data/:type', component: Data}
 ]
 
 const router = new VueRoutes({routes})
