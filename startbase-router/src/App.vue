8cc481bd04142db5da81a8e521a492fa9a4342f4
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-18 14:29:30 -0500

    UPDATE: adding the router link

--- startbase-router/src/App.vue
+++ startbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <div id="app">
-        {{welcome}}
+    <div id="app" class="container mt-5">
+        <h1>{{welcome}}</h1>
+        <h4>
+            <router-link to="/data/people">Meet the characters</router-link> |
+        <router-link to="/data/planet">Explore the planets</router-link>
+        </h4>        
         <router-view></router-view>
     </div>
 </template>
