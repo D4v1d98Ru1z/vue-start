1b05fe5fd75d4c766e3806ff1843c8762603c9db
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-18 14:40:32 -0500

    UPDATE: adding styles

--- startbase-router/src/App.vue
+++ startbase-router/src/App.vue
@@ -2,8 +2,8 @@
     <div id="app" class="container mt-5">
         <h1>{{welcome}}</h1>
         <h4>
-            <router-link to="/data/people">Meet the characters</router-link> |
-        <router-link to="/data/planet">Explore the planets</router-link>
+            <router-link class="link" to="/data/people">Meet the characters</router-link> |
+            <router-link class="link" to="/data/planet">Explore the planets</router-link>
         </h4>        
         <router-view></router-view>
     </div>
