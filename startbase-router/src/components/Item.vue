b4175c29d174174901eff6ceda698996c4283e5b
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-20 12:01:32 -0500

    UPDATE: add some styles

--- startbase-router/src/components/Item.vue
+++ startbase-router/src/components/Item.vue
@@ -2,14 +2,14 @@
    <div class="col-md-4 mb-3">
     <div class="card card-bord">
       <div class="card-body">
-        <h5 class="card-title">{{item.name}}</h5>
+        <h5 class="card-title mb-4">{{item.name}}</h5>
         <h6 class="card-subtitle mb-2 text-muted"></h6>
         <div v-for="(value, keyName, index) in item">
             <div v-if="index < 5">
                 <p class="card-text"><strong>{{keyName}}</strong>: {{value}}</p>
             </div>
         </div>
-        <button type="button" class="btn btn-success">Change {{this.type}}</button>
+        <button type="button" class="btn mt-4 btn-success">Change {{this.type}}</button>
       </div>
     </div>
   </div>
