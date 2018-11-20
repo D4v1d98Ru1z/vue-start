bad5d02e2d7b917dfe65b345f14eb8dbe209e115
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-20 11:52:31 -0500

    UPDATE: adding the name of route to the button

--- startbase-router/src/components/Data.vue
+++ startbase-router/src/components/Data.vue
@@ -23,7 +23,7 @@
                 let list_items = [1, 13, 20]
                 for (let id in list_items){
                     let ids_selected = list_items[id]
-                    console.log(ids_selected)
+                    console.log(ids_selected, this.type)
                     fetch(`https://swapi.co/api/${this.type}/${ids_selected}`, {
                             method: 'GET'
                         }).then(response => response.json()).then(json => this.items.push(json))
--- startbase-router/src/components/Item.vue
+++ startbase-router/src/components/Item.vue
@@ -3,15 +3,21 @@
     <div class="card card-bord">
       <div class="card-body">
         <h5 class="card-title">{{item.name}}</h5>
-        <h6 class="card-subtitle mb-2 text-muted"></h6>
+        <h6 class="card-subtitle mb-2 text-muted">{{item}}</h6>
         <p class="card-text"></p>
-        <button @click="switchCharacter" type="button" class="btn btn-success">Change Character</button>
+        <button @click="switchCharacter" type="button" class="btn btn-success">Change {{this.type}}</button>
       </div>
     </div>
   </div>
 </template>
 <script>
     export default{
-        props: ['item']
+        props: ['item'],
+        data(){
+            return{
+                type: this.$route.params.type
+            }
+        }
+        
     }
 </script>
