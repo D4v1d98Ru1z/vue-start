ffb61a463897abf435614f0b13615f19ea0e6afb
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-20 11:59:08 -0500

    UPDATE: show data from API in the cards

--- startbase-router/src/components/Item.vue
+++ startbase-router/src/components/Item.vue
@@ -3,8 +3,12 @@
     <div class="card card-bord">
       <div class="card-body">
         <h5 class="card-title">{{item.name}}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">{{item}}</h6>
-        <p class="card-text"></p>
+        <h6 class="card-subtitle mb-2 text-muted"></h6>
+        <div v-for="(value, keyName, index) in item">
+            <div v-if="index < 5">
+                <p class="card-text"><strong>{{keyName}}</strong>: {{value}}</p>
+            </div>
+        </div>
         <button type="button" class="btn btn-success">Change {{this.type}}</button>
       </div>
     </div>
@@ -17,7 +21,6 @@
             return{
                 type: this.$route.params.type
             }
-        }
-        
+        }       
     }
 </script>
