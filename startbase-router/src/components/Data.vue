d235f1cd2dc3d685130d8acb29d7f286dd618902
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-22 12:42:49 -0500

    UPDATE: user choose the character

--- startbase-router/src/components/Data.vue
+++ startbase-router/src/components/Data.vue
@@ -1,10 +1,25 @@
 <template>
     <div class="row d-flex">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" :placeholder="'Enter the number of your favorite '+ this.type" aria-label="Recipient's username" aria-describedby="button-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        <div class="container">
+            <div class="input-group mb-3">
+                <input type="number" class="form-control" id="in" :placeholder="'Enter the number of your favorite '+ this.type" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    
+                </div>
             </div>
+            <div class="input-group mb-3">
+                <input type="number" class="form-control" id="in1" :placeholder="'Enter the number of your favorite '+ this.type" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    
+                </div>
+            </div>
+            <div class="input-group mb-3">
+                <input type="number" class="form-control" id="in2" :placeholder="'Enter the number of your favorite '+ this.type" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    
+                </div>
+            </div>
+            <button @click="fetchItems" class="btn btn-outline-secondary mb-3" type="button" id="button-addon2">Save</button>
         </div>
         <Item v-for="(item, index) in items" key="index" :passed-item="item" :type="type" />
     </div>  
@@ -26,7 +41,10 @@
             fetchItems(){
                 this.items = []
                 this.type = this.$route.params.type
-                let list_items = [1, 13, 20]
+                let list_items = []
+                list_items[0] = document.getElementById("in").value
+                list_items[1] = document.getElementById("in1").value
+                list_items[2] = document.getElementById("in2").value
                 for (let id in list_items){
                     let ids_selected = list_items[id]
                     console.log(ids_selected, this.type)
@@ -35,9 +53,6 @@
                         }).then(response => response.json()).then(json => this.items.push(json))
                 }
             }
-        },
-        created() {
-            this.fetchItems()
         },
         components:{
             Item
