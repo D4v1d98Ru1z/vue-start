d7e12aa82006c6c4e6dd47b535287e0fb499b128
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-21 23:52:27 -0500

    UPDATE: showing the data in random way

--- startbase-router/src/components/Item.vue
+++ startbase-router/src/components/Item.vue
@@ -9,18 +9,28 @@
                 <p class="card-text"><strong>{{keyName}}</strong>: {{value}}</p>
             </div>
         </div>
-        <button type="button" class="btn mt-4 btn-success">Change {{this.type}}</button>
+        <button type="button" @click="switchItem" class="btn mt-4 btn-success">Change {{this.type}}</button>
       </div>
     </div>
   </div>
 </template>
 <script>
     export default{
-        props: ['item'],
+        props: ['passedItem', 'type'],
         data(){
             return{
-                type: this.$route.params.type
+                item:{}
             }
-        }       
+        },
+        methods:{
+            switchItem(){
+                let random_id = Math.floor(Math.random()*63)+1
+                fetch(`https://swapi.co/api/${this.type}/${random_id}`, {method: "GET"
+                }).then(response => response.json()).then(json => this.item = json)
+            }
+        },
+        created(){
+            this.item = this.passedItem
+        } 
     }
 </script>
